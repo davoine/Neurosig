6d6d932d9ecacba93578ae9951694a8f7c4f9af7
davoine <federico.davoine@gmail.com>
2011-04-28 07:44:23 -0300

    Liu et al 2008 andando: modeloLiuetal2008.m y Liuetal2008again.m

--- Liuetal2008Chandler99.m
+++ Liuetal2008Chandler99.m
@@ -1,14 +1,35 @@
-function [t,y] = Liuetal2008(tfinal)
+function [t,y] = Liuetal2008Chandler99(tfinal)
 % Basado en "Transition between two excitabilities in mesencephalic V
 % neurons" Yihui Liu & Jing Yang & Sanjue Hu
 % J Comput Neurosci (2008) 24:95–104
+% y Chandler (1999)
+
+%% Parametros
+% 
+% % Potencial de Nernst (en mV)
+% E_K = -92;
+% E_Na = 50;
+% E_L = -72;
+% E_h = -40.8;
+% 
+% 
+% % Conductancias maximas (en uS)
+% g_DRK = 0.6e-3;
+% g_4AP = 0.6e-3;
+% g_h = 2.4e-3;
+% g_Na_t = 1.08e-3;
+% g_Na_p = 72e-6;
+% g_L = 120e-6;
+% 
+% % Capacitancia (en nF)
+% C = 60e-3; % 60 pF
 
 %% Resolucion de la ecuacion diferencial
 tspan = [0 tfinal];
 % Condiciones iniciales
 % y = [V,n_DRK,n_4AP,h_t,s]
 y0 = [-60 0 0 0 0];
-options = odeset('RelTol',1e-9,'AbsTol',[1e-8 1e-5 1e-5 1e-5 1e-5],'Refine',10);
+options = odeset('RelTol',1e-9,'AbsTol',[1e-8 1e-5 1e-5 1e-5 1e-5]);
 [t, y] = ode45(@modeloLiuetal2008,tspan,y0,options);
 
 % Variables de estado
@@ -18,84 +39,37 @@ n_4AP = y(:,3);
 h_t = y(:,4);
 s = y(:,5);
 
-%% Parametros
-
-% Potencial de Nernst (en mV)
-E_K = -92;
-E_Na = 50;
-E_L = -72;
-E_h = -40.8;
-
-% Conductancias maximas (en uS)
-g_DRK = 0.6e-3;
-g_4AP = 0.6e-3;
-g_h = 2.4e-3;
-g_Na_t = 1.08e-3;
-g_Na_p = 7.2e-5;
-g_L = 1.2e-4;
-
-% g_DRK = 0;
-% g_4AP = 0;
-% g_h = 0;
-% g_Na_t = 0;
-% g_Na_p = 0;
-% g_L = 0;
-% Capacitancia (en nF)
-C = 60e-3; % 60 pF
-
-
-% Curvas de activacion e inactivacion
-n_DRK_inf = 1./(1+exp(-(V+4.2)/12.9));
-n_4AP_inf = 1./(1+exp(-(V+43)/3.9));
-m_t_inf = 1./(1+exp(-(V+31.3)/4.3));
-h_t_inf = 1./(1+exp((V+55)/7.1));
-m_p_inf =  1./(1+exp(-(V+50)/6.4));
-s_inf = 1./(1+exp((V+105)/7.3));
-
-% Corrientes
-I_L = g_L.*(V-E_L);
-I_DRK = g_DRK.*n_DRK.*(V-E_K);
-I_4AP = g_4AP.*n_4AP.*(V-E_K);
-I_Na_p = g_Na_p.*m_p_inf.*(V-E_Na);
-I_Na_t = g_Na_t*m_t_inf.*h_t.*(V-E_Na);
-I_h = g_h*s.*(V-E_h);
-
-
-
 % I_inyectada = 1000*(1+square((t-10)/(2*pi),20));
-% I_inyectada = 0;
+I_inyectada = 0;
 % I =20*(1+chirp(t,0,tfinal,100));
 %% Figuras
 figure(1)
 plot(t,y(:,1),'k')
 hold on
-% plot(t,I_inyectada,'b')
+plot(t,I_inyectada,'b')
 hold off
 xlabel('tiempo (ms)')
-% legend('potencial de membrana V_{m} (mV)','corriente inyectada I (nA)')
-
-figure(2)   
-plot(t,y(:,2),'g')
-hold on
-plot(t,y(:,3),'r')
-plot(t,y(:,4),'b')
-plot(t,y(:,5),'k')
-hold off
-xlabel('tiempo')
-ylabel('porcentaje')
-legend('n_{DRK}','n_{4-AP}','h_T','s')
-
-figure(3)
-plot(t,I_L,'g')
-hold on
-plot(t,I_Na_t,'r')
-plot(t,I_Na_p,'b')
-plot(t,I_4AP,'k.')
-plot(t,I_DRK,'k*')
-plot(t,I_h,'k-')
-hold off
-xlabel('tiempo')
-legend('I_L','I_{NaT}','I_{NaP}','I_{4-AP}','I_{DRK}','I_{h}')
+legend('potencial de membrana V_{m} (mV)','corriente inyectada I (nA)')
+% 
+% figure(2)
+% plot(t,y(:,2),'g')
+% hold on
+% plot(t,y(:,3),'r')
+% plot(t,y(:,4),'b')
+% plot(t,y(:,5),'k')
+% hold off
+% xlabel('tiempo')
+% ylabel('porcentaje')
+% legend('n_{DRK}','n_{4-AP}','h_T','s')
+% 
+% % figure(3)
+% % plot(t,I_K,'g')
+% % hold on
+% % plot(t,I_Na_t,'r')
+% % plot(t,I_Na_p,'b')
+% % hold off
+% % xlabel('tiempo')
+% % legend('I_K','I_{NaT}','I_{NaP}')
 
 %% Funcion con las ecuaciones diferenciales
 function dydt = modeloLiuetal2008(t,y)
@@ -122,21 +96,15 @@ E_L = -72;
 E_h = -40.8;
 
 % Conductancias maximas (en uS)
-g_DRK = 0.6e-3;
-g_4AP = 0.6e-3;
-g_h = 2.4e-3;
+g_DRK = 45e-3;
+g_4AP = 8.3e-3;
+g_h = 20.2e-3;
 g_Na_t = 1.08e-3;
-g_Na_p = 7.2e-5;
-g_L = 1.2e-4;
+g_Na_p = 72e-6;
+g_L = 3e-3;
 
-% g_DRK = 0;
-% g_4AP = 0;
-% g_h = 0;
-% g_Na_t = 0;
-% g_Na_p = 0;
-% g_L = 0;
 % Capacitancia (en nF)
-C = 600e-3; % 60 pF
+C = 60e-3; % 60 pF
 
 
 % Curvas de activacion e inactivacion
@@ -149,11 +117,11 @@ n_4AP_inf = 1/(1+exp(-(V+43)/3.9));
 m_t_inf = 1/(1+exp(-(V+31.3)/4.3));
 
 h_t_inf = 1/(1+exp((V+55)/7.1));
-tau_h_t = 15;
+tau_h_t = 1.5;
 
 m_p_inf =  1/(1+exp(-(V+50)/6.4));
 
-s_inf = 1/(1+exp((V+105)/7.3));
+s_inf = 1/(1+exp(-(V+50)/6.4));
 tau_s = 100;
 
 % Corrientes
@@ -166,7 +134,7 @@ I_h = g_h*s*(V-E_h);
 
 % Intensidad en nA
 % I = 100*(1+square((t-10)/(2*pi),20));
-I = -0.1;
+I = 200*t;
 % I =20*(1+chirp(t,0,tfinal,100));
 
 %% Ecuaciones diferenciales
---
+++ Liuetal2008again.m
@@ -0,0 +1,79 @@
+tfinal = 500;
+tspan = [0 tfinal];
+% y = [V, n_DRK, n_4AP, h_t, s]
+y0 = [-60 0 0 0 0];
+[t, y] = ode45(@modeloLiuetal2008, tspan, y0);
+
+% Variables de estado
+V = y(:,1);
+n_DRK = y(:,2);
+n_4AP = y(:,3);
+h_T = y(:,4);
+s = y(:,5);
+
+%% Parametros
+
+% Potencial de Nernst (en mV)
+E_K = -92;
+E_Na = 50;
+E_L = -72;
+E_h = -40.8;
+
+% Conductancias maximas (en uS)
+g_DRK = 0.6e-3;
+g_4AP = 0.6e-3;
+g_h = 2.4e-3;
+g_NaT = 1.08e-3;
+g_NaP = 7.2e-5;
+g_L = 1.2e-4;
+
+% Capacitancia (en nF)
+C = 60e-3; % 60 pF
+
+% Curvas de activacion e inactivacion
+n_DRK_inf = 1./(1+exp(-(V+4.2)/12.9));
+n_4AP_inf = 1./(1+exp(-(V+43)/3.9));
+m_T_inf = 1./(1+exp(-(V+31.3)/4.3));
+h_T_inf = 1./(1+exp((V+55)/7.1));
+m_P_inf =  1./(1+exp(-(V+50)/6.4));
+s_inf = 1./(1+exp((V+105)/7.3));
+
+% Corrientes
+I_L = g_L.*(V-E_L);
+I_DRK = g_DRK.*n_DRK.*(V-E_K);
+I_4AP = g_4AP.*n_4AP.*(V-E_K);
+I_NaP = g_NaP.*m_P_inf.*(V-E_Na);
+I_NaT = g_NaT.*m_T_inf.*h_T.*(V-E_Na);
+I_h = g_h*s.*(V-E_h);
+
+%% Figuras
+figure(1)
+plot(t,y(:,1),'k')
+hold on
+% plot(t,I_inyectada,'b')
+hold off
+xlabel('tiempo (ms)')
+% legend('potencial de membrana V_{m} (mV)','corriente inyectada I (nA)')
+
+figure(2)   
+plot(t,y(:,2),'g')
+hold on
+plot(t,y(:,3),'r')
+plot(t,y(:,4),'b')
+plot(t,y(:,5),'k')
+hold off
+xlabel('tiempo')
+ylabel('porcentaje')
+legend('n_{DRK}','n_{4-AP}','h_T','s')
+
+figure(3)
+plot(t,I_L,'g')
+hold on
+plot(t,I_NaT,'r')
+plot(t,I_NaP,'b')
+plot(t,I_4AP,'k.')
+plot(t,I_DRK,'k*')
+plot(t,I_h,'k-')
+hold off
+xlabel('tiempo')
+legend('I_L','I_{NaT}','I_{NaP}','I_{4-AP}','I_{DRK}','I_{h}')
